4bd2fc4fe0c5ca2d82f562791fcbd4992a0b1b16
nandhini <nandhini@DESKTOP-UCNVJSL>
2025-10-30 14:16:32 +0530

    Assignment 6

--- src/comtnsif/Assignment5.java
+++
@@ -1,5 +0,0 @@
-package comtnsif;
-
-public class Assignment5 {
-
-}
---
+++ src/comtnsif/Assignmentsix/Student.java
@@ -0,0 +1,21 @@
+package comtnsif.Assignmentsix;
+
+public class Student {
+    private int score;
+
+    public Student(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void display() {
+        System.out.println(score);
+    }
+}
